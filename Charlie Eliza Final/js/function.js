9944290faddb76a1b4071a552ac2a32b3d687676
Charlie <Charlie@Chalies-MacBook.local>
2018-12-05 21:25:31 -0800

    loop through square positions

--- Charlie Eliza Final/js/function.js
+++ Charlie Eliza Final/js/function.js
@@ -1,29 +1,33 @@
-var gameBoardOutline = [
-  [1, 2, 3, 4, 5, 6],
-  [7, 8, 9, 10, 11, 12],
-  [13, 14, 15, 16, 17, 18],
-  [19, 20, 21, 22, 23, 24],
-  [25, 26, 27, 28, 29, 30],
-  [31, 32, 33, 34, 35, 36],
-]
+
 
 var gameBoard = [
-  [1, 2, 3, 4, 5, 6],
-  [7, 8, 9, 10, 11, 12],
-  [13, 14, 15, 16, 17, 18],
-  [19, 20, 21, 22, 23, 24],
-  [25, 26, 27, 28, 29, 30],
-  [31, 32, 33, 34, 35, 36],
-]
+  [{x:2, y:2}, {x:102, y:2}, {x:202, y:2}, {x:302, y:2}, {x:402, y:2}, {x:502, y:2}],
+  [{x:2, y:102}, {x:102, y:102}, {x:202, y:102}, {x:302, y:102}, {x:402, y:102}, {x:502, y:102}],
+  [{x:2, y:202}, {x:102, y:202}, {x:202, y:202}, {x:302, y:202}, {x:402, y:202}, {x:502, y:202}],
+  [{x:2, y:302}, {x:102, y:302}, {x:202, y:302}, {x:302, y:302}, {x:402, y:302}, {x:502, y:302}],
+  [{x:2, y:402}, {x:102, y:402}, {x:202, y:402}, {x:302, y:402}, {x:402, y:402}, {x:502, y:402}],
+  [{x:2, y:502}, {x:102, y:502}, {x:202, y:502}, {x:302, y:502}, {x:402, y:502}, {x:502, y:502}]
+];
 
-
-
+var black = 0
+var white = 255
+var color = 0
 
 
 function setup() {
   // set canvas size
   createCanvas(605, 605);
 };
+
+// function mousePressed() {
+//   if (color === black) {
+//     color = white;
+//   } else {
+//     color = black;
+//   }
+//   console.log(color)
+// }
+
 
 function draw() {
   // set background color
@@ -36,65 +40,34 @@ function draw() {
   stroke(0, 0, 0);
   strokeWeight(5);
 
-// row 1
-  rect(2, 2, 100, 100);
-  rect(102, 2, 100, 100);
-  rect(202, 2, 100, 100);
-  rect(302, 2, 100, 100);
-  rect(402, 2, 100, 100);
-  rect(502, 2, 100, 100);
+  gameBoard.forEach(handleRow);
 
-// row 2
-  rect(2, 102, 100, 100);
-  rect(102, 102, 100, 100);
-  rect(202, 102, 100, 100);
-  rect(302, 102, 100, 100);
-  rect(402, 102, 100, 100);
-  rect(502, 102, 100, 100);
+  function handleRow(item) {
+    item.forEach(drawSquare);
+  };
 
-// row 3
-  rect(2, 202, 100, 100);
-  rect(102, 202, 100, 100);
-  rect(202, 202, 100, 100);
-  rect(302, 202, 100, 100);
-  rect(402, 202, 100, 100);
-  rect(502, 202, 100, 100);
+  function drawSquare(item) {
+    rect(item.x, item.y, 100, 100)
+  };
 
-// row 4
-  rect(2, 302, 100, 100);
-  rect(102, 302, 100, 100);
-  rect(202, 302, 100, 100);
-  rect(302, 302, 100, 100);
-  rect(402, 302, 100, 100);
-  rect(502, 302, 100, 100);
-
-// row 5
-  rect(2, 402, 100, 100);
-  rect(102, 402, 100, 100);
-  rect(202, 402, 100, 100);
-  rect(302, 402, 100, 100);
-  rect(402, 402, 100, 100);
-  rect(502, 402, 100, 100);
-
-// row 6
-  rect(2, 502, 100, 100);
-  rect(102, 502, 100, 100);
-  rect(202, 502, 100, 100);
-  rect(302, 502, 100, 100);
-  rect(402, 502, 100, 100);
-  rect(502, 502, 100, 100);
-
-
-  noStroke();
-  stroke(255, 255, 255);
-  strokeWeight(3);
-  fill(0, 0, 0);
-  ellipse(52, 52, 85, 85);
-
-
-  noStroke();
-  stroke(0, 0, 0);
-  strokeWeight(3);
-  fill(255, 255, 255);
-  ellipse(152, 152, 85, 85);
+  // noStroke();
+  // stroke(255, 255, 255);
+  // strokeWeight(3);
+  // fill(0, 0, 0);
+  // ellipse(52, 52, 85, 85);
+  //
+  //
+  // noStroke();
+  // stroke(0, 0, 0);
+  // strokeWeight(3);
+  // fill(255, 255, 255);
+  // ellipse(152, 152, 85, 85);
 };
+
+function mouseClicked() {
+  ellipse(150, 150, 85, 85);
+  draw();
+  // prevent default
+  // return false;
+  console.log();
+}
